5b02d1c800744d0b6980d7c35448eb80a1b84032
RustamZakAs <rostik055@gmail.com>
2022-12-12 20:08:02 +0400

    changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace KillProcesses
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -34,8 +34,10 @@ namespace KillProcesses
             "asdfghdf"}, -1);
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.rbProcesses = new System.Windows.Forms.RadioButton();
+            this.rbServices = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // listView1
@@ -56,33 +58,59 @@ namespace KillProcesses
             // 
             this.columnHeader1.Width = 257;
             // 
-            // button1
+            // btnLoad
             // 
-            this.button1.Location = new System.Drawing.Point(301, 42);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Load";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnLoad.Location = new System.Drawing.Point(301, 42);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
             // 
-            // textBox1
+            // tbSearch
             // 
-            this.textBox1.Location = new System.Drawing.Point(43, 71);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(333, 20);
-            this.textBox1.TabIndex = 2;
+            this.tbSearch.Location = new System.Drawing.Point(43, 71);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(333, 20);
+            this.tbSearch.TabIndex = 2;
+            this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
             // 
-            // Form1
+            // rbProcesses
+            // 
+            this.rbProcesses.AutoSize = true;
+            this.rbProcesses.Checked = true;
+            this.rbProcesses.Location = new System.Drawing.Point(43, 45);
+            this.rbProcesses.Name = "rbProcesses";
+            this.rbProcesses.Size = new System.Drawing.Size(74, 17);
+            this.rbProcesses.TabIndex = 3;
+            this.rbProcesses.TabStop = true;
+            this.rbProcesses.Text = "Processes";
+            this.rbProcesses.UseVisualStyleBackColor = true;
+            // 
+            // rbServices
+            // 
+            this.rbServices.AutoSize = true;
+            this.rbServices.Location = new System.Drawing.Point(134, 45);
+            this.rbServices.Name = "rbServices";
+            this.rbServices.Size = new System.Drawing.Size(66, 17);
+            this.rbServices.TabIndex = 4;
+            this.rbServices.TabStop = true;
+            this.rbServices.Text = "Services";
+            this.rbServices.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(416, 348);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rbServices);
+            this.Controls.Add(this.rbProcesses);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.btnLoad);
             this.Controls.Add(this.listView1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "FormMain";
+            this.Text = "Processes & Services";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -93,8 +121,10 @@ namespace KillProcesses
 
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.RadioButton rbProcesses;
+        private System.Windows.Forms.RadioButton rbServices;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,13 +5,14 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KillProcesses
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
         private bool isRunned = false;
         public bool IsRunned
@@ -20,7 +21,7 @@ namespace KillProcesses
             set { isRunned = value; }
         }
 
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
         }
@@ -32,9 +33,11 @@ namespace KillProcesses
         }
 
         List<Process> processes;
+        List<ServiceController> services;
         private void LoadProcesses()
         {
             this.IsRunned = true;
+            this.listView1.Items.Clear();
             task = new Task(() => {
                 processes = Process.GetProcesses().ToList();
                 processes.ForEach(item => {
@@ -49,10 +52,111 @@ namespace KillProcesses
             task.Start();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void LoadServices()
+        {
+            this.IsRunned = true;
+            this.listView1.Items.Clear();
+            task = new Task(() => {
+                services = ServiceController.GetServices().ToList();
+                services.ForEach(item => {
+                    if (!this.listView1.InvokeRequired)
+                        this.listView1.Items.Add(new ListViewItem(item.ServiceName));
+                    else this.listView1.Invoke(new Action(() => {
+                        this.listView1.Items.Add(new ListViewItem(item.ServiceName));
+                    }));
+                });
+                this.IsRunned = false;
+            });
+            task.Start();
+        }
+
+        public static bool IsServiceInstalled(string serviceName)
+        {
+            // get list of Windows services
+            ServiceController[] services = ServiceController.GetServices();
+
+            // try to find service name
+            foreach (ServiceController service in services)
+                if (service.ServiceName == serviceName)
+                    return true;
+            return false;
+        }
+
+        public static void StartWindowsService(string serviceName)
+        {
+            ServiceController serviceController = new ServiceController(serviceName);
+            TimeSpan timeout = TimeSpan.FromMilliseconds(1000);
+            serviceController.Start();
+            serviceController.WaitForStatus(ServiceControllerStatus.Running, timeout);
+        }
+
+        public static void StopWindowsService(string serviceName)
+        {
+            ServiceController serviceController = new ServiceController(serviceName);
+            TimeSpan timeout = TimeSpan.FromMilliseconds(1000);
+            serviceController.Stop();
+            serviceController.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+        }
+
+        public static void RestartWindowsService(string serviceName)
+        {
+            ServiceController serviceController = new ServiceController(serviceName);
+            int tickCount1 = Environment.TickCount;
+            int tickCount2 = Environment.TickCount;
+            TimeSpan timeout = TimeSpan.FromMilliseconds(1000);
+            serviceController.Stop();
+            serviceController.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+
+            timeout = TimeSpan.FromMilliseconds(1000 - (tickCount1 - tickCount2));
+            serviceController.Start();
+            serviceController.WaitForStatus(ServiceControllerStatus.Running, timeout);
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
         {
             if (!this.IsRunned)
-                LoadProcesses();
+            {
+                if (this.rbProcesses.Checked)
+                    LoadProcesses();
+                else if (this.rbServices.Checked)
+                    LoadServices();
+            }
+        }
+
+        private void tbSearch_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string searchedText = this.tbSearch.Text;
+                this.listView1.Items.Clear();
+                if (this.rbProcesses.Checked)
+                {
+                    var temp = processes.Where(x => x.ProcessName.Contains(searchedText)).ToList();
+                    temp.ForEach(item => {
+                        if (!this.listView1.InvokeRequired)
+                            this.listView1.Items.Add(new ListViewItem(item.Id + '-' + item.ProcessName));
+                        else this.listView1.Invoke(new Action(() => {
+                            this.listView1.Items.Add(new ListViewItem(item.Id + '-' + item.ProcessName));
+                        }));
+                    });
+                }
+                else if (this.rbServices.Checked)
+                {
+                    var temp = services.Where(x => x.ServiceName.Contains(searchedText)).ToList();
+                    temp.ForEach(item => {
+                        if (!this.listView1.InvokeRequired)
+                            this.listView1.Items.Add(new ListViewItem(item.ServiceName));
+                        else this.listView1.Invoke(new Action(() => {
+                            this.listView1.Items.Add(new ListViewItem(item.ServiceName));
+                        }));
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return;
+                throw;
+            }
         }
     }
 }
